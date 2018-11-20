b10aca2b23ad8c5400721a1937fec4c5f9100903
d.caceres7 <dancacers_95@unitec.edu>
2018-11-20 12:36:50 -0600

    CRUD Profesor

--- src/proyectortbdii/Redis.java
+++ src/proyectortbdii/Redis.java
@@ -3,7 +3,6 @@ package proyectortbdii;
 import java.util.HashMap;
 import java.util.Map;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.exceptions.JedisException;
 
 public class Redis {
@@ -29,9 +28,7 @@ public class Redis {
         
     }
     
-    public void insertProfesor(Profesor profesor) {
-        //System.out.println("Server Ping: " + jedis.ping());
-
+    public void createProfesor(Profesor profesor) {
         Map<String, String> userProperties = new HashMap<String, String>();
         userProperties.put("id", profesor.getId());
         userProperties.put("nombre", profesor.getNombre());
@@ -41,34 +38,41 @@ public class Redis {
         userProperties.put("telefono", profesor.getTelefono());
         userProperties.put("sueldo", profesor.getSueldo());
         userProperties.put("experiencia", profesor.getExperiencia());
-        
         try{
             jedis.hmset(profesor.getId().toString(), userProperties);
-            System.out.println("ID: "+profesor.getId());
-            
         } catch (JedisException e) {
-            //if something wrong happen, return it back to the pool
-           System.out.println("Error");
+           System.out.println("Error" + e);
         } 
 
         jedis.close();
     }
 
-    public void updateTelefono(Profesor profesor) {
-        Jedis jedis = new Jedis("localhost");
-        System.out.println("Connection Successful");
-        System.out.println("Server Ping: " + jedis.ping());
-
-        
-        jedis.hset("profesor:" + profesor.getId(), "telefono", profesor.getTelefono());
-
+    public void updateProfesor(Profesor profesor) {
+        Map<String, String> userProperties = new HashMap<String, String>();
+        userProperties.put("id", profesor.getId());
+        userProperties.put("nombre", profesor.getNombre());
+        userProperties.put("apellido", profesor.getApellido());
+        userProperties.put("genero", profesor.getGenero());
+        userProperties.put("fecha_nacimiento", profesor.getFecha_nacimiento());
+        userProperties.put("telefono", profesor.getTelefono());
+        userProperties.put("sueldo", profesor.getSueldo());
+        userProperties.put("experiencia", profesor.getExperiencia());
+        try{
+            jedis.hmset(profesor.getId().toString(), userProperties);
+        } catch (JedisException e) {
+           System.out.println("Error" + e);
+        } 
         
         jedis.close();
     }
 
-    public Map<String, String> loadProfesor(String id) {
+    public Map<String, String> readProfesor(String id) {
         Map<String, String> properties = jedis.hgetAll(id);
         jedis.close();
         return properties;
+    }
+    
+    public void deleteProfesor(String id){
+        jedis.del(id);
     }
 }
